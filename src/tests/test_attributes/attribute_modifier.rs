51d67b30e6cac1840bf6bab02794653314b275cc
Patrick Unick <storm1surge@gmail.com>
2024-03-21 18:05:21 -1000

    [uni] Update tests

--- src/tests/test_attributes/attribute_modifier.rs
+++ src/tests/test_attributes/attribute_modifier.rs
@@ -25,23 +25,3 @@ fn parse_minus_modifier() {
 
     assert!(matches!(parsed, Ok(AttributeModifier::Minus(_))));
 }
-
-#[test]
-fn parse_plus_modifier_with_depth() {
-    let input = quote!{+ 5};
-    let parsed = parse_str::<AttributeModifier>(&input.to_string()).expect("Should parse correctly");
-
-    match parsed {
-        AttributeModifier::Plus { depth, .. } => assert_eq!(depth, 5),
-        _ => panic!("Expected Plus variant"),
-    }
-}
-
-#[test]
-fn parse_plus_modifier_with_invalid_depth() {
-    let input = quote!{+not_a_number};
-    let parsed = parse_str::<AttributeModifier>(&input.to_string());
-
-    assert!(parsed.is_err()); // Expecting an error due to invalid depth
-}
-
--- tests/test.rs
+++ tests/test.rs
@@ -3,20 +3,25 @@
 use nestify2::nest;
 
 #[test]
+fn es() {}
+
+#[test]
 fn please_work() {
+    let s = 32;
     nest!(
+        #[derive(Debug)]
         struct Hello {
-            pub o: struct Another {
-                s: struct A {
+
+            pub o: 
+            struct Another {
+                s: #[derive(Debug)] struct A {
                     a: struct B {
                         asdfs: struct BB {
 
                         },
                     },
                     c: i32,
-                    d: struct D {
-
-                    },
+                    d: struct D();,
                     asd: Option<i32>,
                 },
             },
@@ -28,11 +33,12 @@ fn please_work() {
 #[test]
 fn enum_test() {
     nest! {
+        #[derive(Debug)]*
         enum Outside {
             Empty,
             Tuple(Option<i32>, i32, enum Nested1 { }),
             Named {
-                a: i32,
+                a: (),
                 b: Option<i32>,
                 c: enum Nested2 {
                     Tuple2(enum Nested3 {})
@@ -47,10 +53,23 @@ fn attribute_test() {
     nest! {
         #[derive(Default)]
         enum AnEnum {
-            One,
             #[default]
+            One = 1,
             Default,
             Two,
         }
     }
+}
+
+#[test]
+fn test_semis() {
+
+}
+
+#[derive(Default)]
+enum AnEnum {
+    One = 1,
+    #[default]
+    Default,
+    Two,
 }
